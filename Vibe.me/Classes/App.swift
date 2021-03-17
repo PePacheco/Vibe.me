5708822f61f333dc0fb5d3fb6c129afa44bb2d7c
Santiago <santiago.dgonzaga@hotmail.com>
2021-03-17 16:58:32 -0300

    Create a Login System

--- Vibe.me/Classes/App.swift
+++ Vibe.me/Classes/App.swift
@@ -11,15 +11,23 @@ import Combine
 class App {
     var playerManager: PlayerManager
     var users: [User]
+    var loginManager: LoginManager
     
     init() {
         print(Constants.Banners.mainBanner)
         self.playerManager = PlayerManager()
         self.users = boot(filename: "profiles.json")
+        self.loginManager = LoginManager()
     }
     
     func execute() {
-        self.playerManager.execute()
+        if let credentials = readLine() {
+            let credentialsArray = credentials.components(separatedBy: " ")
+            print(credentialsArray)
+            if self.loginManager.login(users: users, username: credentialsArray[0], password: credentialsArray[1]){
+                self.playerManager.execute()
+            }
+        }
     }
     
     
